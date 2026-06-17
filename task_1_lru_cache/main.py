d98b0a1d8dfc983bc0e608f2fac852d31978fe1e
Inna Khomovska <innapariy@gmail.com>
2026-06-17 22:15:15 +0200

    feat(task1): implement custom LRUCache class with invalidation logic

--- task_1_lru_cache/main.py
+++ task_1_lru_cache/main.py
@@ -20,6 +20,38 @@ def make_queries(n,q, hot_pool=30, p_hot=0.95, p_update=0.03):
             queries.append(("Range", left, right))
     return queries
 
+class LRUCache:
+    def __init__(self, capacity: int = 1000):
+        self.capacity = capacity
+        self.cache = {}
+    
+    def get(self, key):
+        if key in self.cache:
+            return -1
+        value = self.cache.pop(key)
+        self.cache[key] = value
+        return value
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache[key]= value
+            return value
+    def put(self, key, value):
+        if key in self.cashe:
+            self.cashe.pop(key)
+        elif len(self.cashe) >= self.capacity:
+            first_key = next(iter(self.cache))
+            self.cashe.pop(first_key)
+        self.cashe[key] = value
+    def invalidate_index(self, index: int):
+        keys_to_remove = [key for key in self.cache.keys() if key[0] <= index <= key[1]]
+        for key in keys_to_remove:
+            self.cache.pop(key) 
+
+cache_sysrtem = LRUCache(capacity=1000)  
+
+        
+
 def range_sum_no_cache(array, left, right):
     return sum(array[left : right + 1])
 
