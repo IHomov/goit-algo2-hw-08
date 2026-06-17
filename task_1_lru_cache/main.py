94998bf65824c528df42d2fd49a62d50915ed328
Inna Khomovska <innapariy@gmail.com>
2026-06-17 21:29:45 +0200

    feat(task1): fix function definitions and verify baseline execution

---
+++ task_1_lru_cache/main.py
@@ -0,0 +1,37 @@
+import random
+import time
+
+#generate requests
+def make_queries(n,q, hot_pool=30, p_hot=0.95, p_update=0.03):
+    hot = [(random.randint(0, n //2), random.randint(n//2, n-1))
+           for _ in range(hot_pool)]
+    queries= []
+    for _ in range(q):
+        if random.random() < p_update:
+            idx = random.randint(0, n-1)
+            val = random.randint(1, 100)
+            queries.append(("Update", idx, val))
+        else:
+            if random.random() < p_hot:
+                left, right = random.choice(hot)
+            else:
+                left = random.randint(0, n-1)
+                right = random.randint(left, n-1)
+            queries.append(("Range", left, right))
+    return queries
+
+def range_sum_no_cache(array, left, right):
+    return sum(array[left : right + 1])
+
+def update_no_cache(array, index, value):
+    array[index] = value
+
+if __name__ == "__main__":
+        test_queries = make_queries(100000, 50000)
+        print("--- Перевірка запуску скрипта ---")
+        test_array = [1, 2, 3, 4, 5]
+        print(f"Масив: {test_array}")
+        print(f"Сума елементів з 1 по 3 індекс: {range_sum_no_cache(test_array, 1, 3)}")
+        update_no_cache(test_array, 2, 10)
+        print(f"Масив після оновлення індексу 2 на значення 10: {test_array}")
+
